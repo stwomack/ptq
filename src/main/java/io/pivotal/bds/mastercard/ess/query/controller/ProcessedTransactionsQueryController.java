769b6829b9f3ec8beba6a2913105b8343fcefc4a
Steve Womack <stwomack@gmail.com>
2016-11-22 12:05:52 -0600

    Adds stringified query

--- src/main/java/io/pivotal/bds/mastercard/ess/query/controller/ProcessedTransactionsQueryController.java
+++ src/main/java/io/pivotal/bds/mastercard/ess/query/controller/ProcessedTransactionsQueryController.java
@@ -1,16 +1,21 @@
 package io.pivotal.bds.mastercard.ess.query.controller;
 
+import io.pivotal.bds.mastercard.ess.query.service.ProcessedTransactionsQueryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 public class ProcessedTransactionsQueryController {
+    @Autowired
+    ProcessedTransactionsQueryService processedTransactionsQueryService;
+
     @RequestMapping(value = "/processedTransactions/{custClstrId}", method = RequestMethod.GET)
     public
     @ResponseBody
-    Map searchWithSearchTerm(@PathVariable("custClstrId") String custClstrId) {
-        return null;
+    Map queryByCustomer(@PathVariable("custClstrId") String custClstrId) {
+        return processedTransactionsQueryService.queryByCustomer(custClstrId);
     }
 
 }
--- src/main/java/io/pivotal/bds/mastercard/ess/query/service/ProcessedTransactionsQueryService.java
+++ src/main/java/io/pivotal/bds/mastercard/ess/query/service/ProcessedTransactionsQueryService.java
@@ -1,7 +1,20 @@
 package io.pivotal.bds.mastercard.ess.query.service;
 
-/**
- * Created by swomack on 11/22/16.
- */
+import io.pivotal.bds.mastercard.ess.query.ProcessedTransactionsQueryDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+
 public class ProcessedTransactionsQueryService {
+
+    @Autowired
+    ProcessedTransactionsQueryDAO processedTransactionsQueryDAO;
+
+    public Map queryByCustomer(String custClstrId) {
+        return processedTransactionsQueryDAO.queryByCustomer(custClstrId);
+    }
+
+    public Map queryByCustomerTest(String custClstrId) {
+        return processedTransactionsQueryDAO.queryByCustomerTest(custClstrId);
+    }
 }
