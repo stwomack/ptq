5ac9fdc1ea6c8fb2c08b4d547194d065e6458383
Steve Womack <stwomack@gmail.com>
2016-11-22 19:00:40 -0600

    ups almost working. pgadmin user needs remote access to gcp instance

--- src/main/resources/scripts.sh
+++ src/main/resources/scripts.sh
@@ -1,2 +1,20 @@
 #!/usr/bin/env bash
 cf cups ess-postgres -p '{"username":"gpadmin","password":"gpadmin","host":"10.253.80.11","port":"5432","dbname":"ess"}'
+
+
+
+cf cups ess-postgres -p "hostname,port,name,username,password,uri,jdbcUrl"
+
+hostname> 10.0.0.230
+
+port> 5432
+
+name> ess
+
+username> gpadmin
+
+password> gpadmin
+
+uri> postgresql://gpadmin:gpadmin@10.0.0.230:5432/ess?reconnect=true
+
+jdbcUrl> jdbc:postgresql://10.0.0.230:5432/ess?user=gpadmin
