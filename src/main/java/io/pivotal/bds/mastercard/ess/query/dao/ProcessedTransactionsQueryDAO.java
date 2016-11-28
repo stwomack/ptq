d40b3047dbe8f5c156e6502bc1b8a97e4cfb8026
Steve Womack <stwomack@gmail.com>
2016-11-28 12:41:16 -0600

    Adds sublist

--- src/main/java/io/pivotal/bds/mastercard/ess/query/dao/ProcessedTransactionsQueryDAO.java
+++ src/main/java/io/pivotal/bds/mastercard/ess/query/dao/ProcessedTransactionsQueryDAO.java
@@ -86,7 +86,7 @@ public class ProcessedTransactionsQueryDAO {
 //                " 19355,\n" +
 //                " 23031)\n" +
             "   AND PT.CUST_CLSTR_ID IN (1, 2, 3)\n" +
-            "   ORDER BY PRCSS_DT_TM LIMIT :limit;";
+            "   ORDER BY PRCSS_DT_TM LIMIT 100;";
 
     public List queryByCustomer(String startDate, String endDate, Long limit) {
         Map namedParameters = new HashMap();
