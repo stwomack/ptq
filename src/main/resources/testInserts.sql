f9ee1288c595ecf3c3024ae826c8325001b1181f
Steve Womack <stwomack@gmail.com>
2016-12-08 15:42:26 -0600

    Changes test people

--- src/main/resources/testInserts.sql
+++ src/main/resources/testInserts.sql
@@ -1,3 +1,3 @@
-insert into people values ('Steve', 'Womack');
-insert into people values ('Ashley', 'Womack');
+insert into people values ('Fyodor', 'Dostoyevsky');
+insert into people values ('Adolf', 'Ghandi');
 insert into people values ('Leo', 'Tolstoy');
