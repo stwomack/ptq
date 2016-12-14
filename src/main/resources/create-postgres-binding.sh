b1ef05b037982908b5a7c6b3e80da78086008b85
Steve Womack <stwomack@gmail.com>
2016-12-14 15:21:08 -0600

    yolo

--- src/main/resources/create-postgres-binding.sh
+++ src/main/resources/create-postgres-binding.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cf cups mess-postgres -p '{"hostname":"10.253.80.11","port":"5432","name":"ess","username":"rdr_user_rl","password":"pivotal","uri":"postgresql://gpadmin:gpadmin@10.253.80.11:5432/mcdb?reconnect=true","jdbcUrl":"jdbc:postgresql://10.253.80.11:5432/mcdb?user=rdr_user_rl"}'
+cf cups ess-postgres -p '{"hostname":"10.253.80.11","port":"5432","name":"ess","username":"rdr_user_rl","password":"pivotal","uri":"postgresql://gpadmin:gpadmin@10.253.80.11:5432/mcdb?reconnect=true","jdbcUrl":"jdbc:postgresql://10.253.80.11:5432/mcdb?user=rdr_user_rl"}'
