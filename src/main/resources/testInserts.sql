e9d8ddc3fa8db9e8382dbf3b4fc4d87a578ee1b8
Steve Womack <stwomack@gmail.com>
2016-12-22 14:11:35 -0600

    yolo

--- src/main/resources/testInserts.sql
+++ src/main/resources/testInserts.sql
@@ -1,3 +1,3 @@
 insert into people values ('Fyodor', 'Dostoyevsky');
-insert into people values ('Adolf', 'Ghandi');
+insert into people values ('Mohatma', 'Ghandi');
 insert into people values ('Leo', 'Tolstoy');
