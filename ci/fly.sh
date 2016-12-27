396074d95de04cb85547468bc633794cbe4c38b8
Steve Womack <stwomack@gmail.com>
2016-12-27 11:26:43 -0600

    yolo

--- ci/fly.sh
+++ ci/fly.sh
@@ -2,6 +2,6 @@
 
 set -e -x
 
-fly -t ci login -c http://192.168.100.4:8080
+fly -t ci login -c -u dnaadmin -p Idkhowaboutpassword1 http://localhost:8080
 fly -t ci set-pipeline --pipeline ptq --config pipeline.yml --load-vars-from .cf-env.yml
 fly -t ci unpause-pipeline --pipeline ptq
--- src/main/java/io/pivotal/bds/mastercard/ess/query/ProcessedTransactionsQueryApplication.java
+++ src/main/java/io/pivotal/bds/mastercard/ess/query/ProcessedTransactionsQueryApplication.java
@@ -1,7 +1,12 @@
 package io.pivotal.bds.mastercard.ess.query;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class ProcessedTransactionsQueryApplication {
@@ -9,4 +14,15 @@ public class ProcessedTransactionsQueryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProcessedTransactionsQueryApplication.class, args);
 	}
+}
+
+@Component
+class ExampleInfoContributor implements InfoContributor {
+
+	@Override
+	public void contribute(Info.Builder builder) {
+		builder.withDetail("example",
+				Collections.singletonMap("key", "value"));
+	}
+
 }
