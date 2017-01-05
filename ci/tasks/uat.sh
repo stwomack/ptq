3b9b394fd7f4bb205f7962baed77a708371b591b
Steve Womack <stwomack@gmail.com>
2017-01-05 17:39:39 -0600

    yolo

--- ci/tasks/uat.sh
+++ ci/tasks/uat.sh
@@ -2,6 +2,10 @@
 
 set -e -x
 
-curl https://ptq.app.52.176.40.186.cf.pcfazure.com/info -k
+RESPONSE = `curl https://ptq.app.52.176.40.186.cf.pcfazure.com/info -k`
+
+if [ $RESPONSE != '{"Property":{"key":"value"}}' ]; then
+  exit -1;
+fi
 
 cp -fr build-output/. uat-output
