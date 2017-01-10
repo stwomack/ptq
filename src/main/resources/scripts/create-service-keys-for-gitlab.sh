706d29330f7f23cedfc50dcee527e16f9c2a6cc9
Administrator <admin@example.com>
2017-01-10 09:19:31 -0600

    yolo

--- src/main/resources/scripts/create-service-keys-for-gitlab.sh
+++ src/main/resources/scripts/create-service-keys-for-gitlab.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 cf create-service-key my-mysql gitlab-mysql
 cf create-service-key my-redis gitlab-redis
-cf service-key my-redis gitlab-redis
-cf service-key my-mysql gitlab-mysql
+cf service-key my-redis gitlab-redis; cf service-key my-mysql gitlab-mysql
