81707b59c130bc220620c16edb0d98fb8a138d1c
Administrator <admin@example.com>
2017-01-12 11:06:16 -0600

    yolo

--- ci/tasks/update-manifest.sh
+++ ci/tasks/update-manifest.sh
@@ -1,30 +1,23 @@
-#!/bin/ash
+#!/bin/bash
 
-set -e -x
+set -e
 
-# args
-inputDir=  outputDir=  versionFile=  artifactId=  packaging=
+echo "Finalizing production deployment..."
+
+# required
+versionFile=  artifactId=
+
+# optional
+hostname=$CF_HOST # default to env variable from pipeline
 
 while [ $# -gt 0 ]; do
   case $1 in
-    -i | --input-dir )
-      inputDir=$2
-      shift
-      ;;
-    -o | --output-dir )
-      outputDir=$2
-      shift
-      ;;
     -v | --version-file )
       versionFile=$2
       shift
       ;;
     -a | --artifactId )
       artifactId=$2
-      shift
-      ;;
-    -p | --packaging )
-      packaging=$2
       shift
       ;;
     * )
@@ -40,25 +33,61 @@ error_and_exit() {
   exit 1
 }
 
-if [ ! -d "$inputDir" ]; then
-  error_and_exit "missing input directory: $inputDir"
-fi
-if [ ! -d "$outputDir" ]; then
-  error_and_exit "missing output directory: $outputDir"
-fi
 if [ ! -f "$versionFile" ]; then
   error_and_exit "missing version file: $versionFile"
 fi
 if [ -z "$artifactId" ]; then
   error_and_exit "missing artifactId!"
 fi
-if [ -z "$packaging" ]; then
-  error_and_exit "missing packaging!"
+
+# copy the war file to the output directory
+version=`cat $versionFile`
+
+# actual app name of new app
+appName="${artifactId}-${version//\./-}"
+
+# route name -- could use different name than the app name (artifactId)
+#routeName="${hostname}-${version//\./-}"
+
+# get latest CF CLI
+curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
+./cf --version
+
+./cf login -a ${CF_API_ENDPOINT} -u ${CF_USER} -p ${CF_PWD} -o ${CF_ORG} -s ${CF_SPACE} --skip-ssl-validation
+
+# list the app names which are deployed using the base app name
+DEPLOYED_APPS=$(./cf apps | grep ${artifactId} | cut -d" " -f1)
+echo "Deployed apps: ${DEPLOYED_APPS}"
+
+# Map app version onto main app route and scale the app to support traffic
+# cf map-route ptq-0-0-5 cfapps.io -n ptq
+echo "map ${appName} to route ${hostname}.${CF_DOMAIN}"
+./cf map-route $appName $CF_DOMAIN -n $hostname
+
+# remove versioned route name
+./cf unmap-route $appName $CF_DOMAIN -n $appName
+
+# cf scale ptq-0-0-5 -i 2
+echo "scaling ${appName} up..."
+./cf scale $appName -i 2
+
+# Scale down, unmap routes, and remove old versions of app, except a basename app = ptq
+if [ ! -z "$DEPLOYED_APPS" -a "$DEPLOYED_APPS" != " " -a "$DEPLOYED_APPS" != "$appName" ]; then
+  echo "Performing zero-downtime cutover to $appName"
+
+  while read -r line
+  do
+    # if the currently deployed apps are not the new app or an app with the base app name
+    if [ ! -z "$line" -a "$line" != " " -a "$line" != "$appName" -a "$line" != "$artifactId" ]; then
+      echo "Scaling down, unmapping and removing app: $line"
+      ./cf scale "$line" -i 1
+      #./cf unmap-route "$line" $CF_DOMAIN -n $hostname
+      ./cf delete "$line" -f
+    else
+      echo "Skipping $line"
+    fi
+  done <<<"$DEPLOYED_APPS"
 fi
 
-version=`cat $versionFile`
-artifactName="${artifactId}-${version}.${packaging}"
-
-# Copy jar file to concourse output folder
-sed -i -- "s/changeme/ptq-${version}.jar/g" "${inputDir}/manifest.yml"
-cp -fr $inputDir/. $outputDir
+# clear up unused routes
+./cf delete-orphaned-routes -f
