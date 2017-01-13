aac1aef1bdc61e837f2f3cfd421497df6d8f9440
Administrator <admin@example.com>
2017-01-13 15:54:37 -0600

    yolo

--- src/main/resources/scripts/cups-azure1.sh
+++ src/main/resources/scripts/cups-azure1.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cf cups ess-postgres -p '{"hostname":"10.128.0.12","port":"5432","name":"mastercard","username":"rdr_user_rl","password":"pivotal","uri":"postgresql://gpadmin:gpadmin@10.128.0.12:5432/mastercard?reconnect=true","jdbcUrl":"jdbc:postgresql://10.128.0.12:5432/mastercard?user=rdr_user_rl"}'
+cf cups ess-postgres -p '{"hostname":"10.1.7.4","port":"5432","name":"mastercard","username":"rdr_user_rl","uri":"postgresql://gpadmin:gpadmin@10.1.7.4:5432/mastercard?reconnect=true","jdbcUrl":"jdbc:postgresql://10.1.7.4:5432/mastercard?user=rdr_user_rl"}'
