477bc0bcefab74f45429bcc7126c441bb6faafea
Administrator <admin@example.com>
2017-01-15 14:45:28 -0600

    yolo

--- ci/fly.sh
+++ ci/fly.sh
@@ -2,7 +2,7 @@
 
 set -e -x
 
-fly -t ci login -u concourse -p changeme -c http://localhost:8080/
+fly -t ci login -u concourse -p changeme -c http://13.92.135.92:8080/
 #fly -t ci login -c http://192.168.100.4:8080/
 
 fly -t ci set-pipeline --pipeline ptq --config pipeline.yml --load-vars-from .cf-env.yml
