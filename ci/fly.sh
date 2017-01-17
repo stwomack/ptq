afa14a283c394778386f0e1960b0044e764eb9d5
Administrator <admin@example.com>
2017-01-16 19:24:56 -0600

    yolo

--- ci/fly.sh
+++ ci/fly.sh
@@ -2,8 +2,8 @@
 
 set -e -x
 
-#fly -t ci login -u concourse -p changeme -c http://13.92.135.92:8080/
-fly -t ci login -c http://192.168.100.4:8080/
+fly -t ci login -u dnaadmin -p Idkhowaboutpassword1 -c http://13.92.135.92:8080/
+#fly -t ci login -c http://192.168.100.4:8080/
 
 fly -t ci set-pipeline --pipeline ptq --config pipeline.yml --load-vars-from .cf-env.yml
 fly -t ci unpause-pipeline --pipeline ptq
